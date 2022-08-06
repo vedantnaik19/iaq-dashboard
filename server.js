ce121095f82eaf9396542f6c13d2377bf32c6bdf
Vedant Naik <vedantnaik@eduroam-22-147.uea.ac.uk>
2022-08-06 04:53:07 +0100

    prod

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
+app.use(express.static('public'));
+app.get("*", (req, res) => {
+  const index = path.join(__dirname, "public", "index.html");
+  res.sendFile(index);
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
- 
 app.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
- });
+  console.log(`Server is up on port ${port}!`);
+});
