f3942af8a1e3dc6e99e2bf6e813c501c72ba65d6
Vedant Naik <vedantnaik@eduroam-23-181.uea.ac.uk>
2022-08-07 20:03:47 +0100

    last updated at

--- src/components/side-bar/side-bar.js
+++ src/components/side-bar/side-bar.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 import "./side-bar.css";
 import { useSites, useUpdateCheckedSites } from "../../contexts/SitesContext";
+import ReactTimeAgo from "react-time-ago";
+
 
 const SideBar = () => {
   const [handleOnChange] = useUpdateCheckedSites();
   const [sites, checkedSites] = useSites();
+  console.log(Date.now());
+
   return (
     <div className="side-bar-container">
       <h5>Sites</h5>
@@ -20,6 +24,12 @@ const SideBar = () => {
             checked={checkedSites.has(site._id)}
           />
           <label className="form-check-label">{site.name}</label>
+          <p>
+            <small class="text-muted">
+              Last updated: <ReactTimeAgo date={site.updatedAt} locale="en-US"/>
+            </small>
+          </p>
+          <span className="space"></span>
         </div>
       ))}
     </div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TimeAgo from "javascript-time-ago";
 
+import en from "javascript-time-ago/locale/en.json";
+import ru from "javascript-time-ago/locale/ru.json";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+TimeAgo.addDefaultLocale(en);
+TimeAgo.addLocale(en);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
